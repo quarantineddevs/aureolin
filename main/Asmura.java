751fcff9baa11e0bede6aae364316e7e60f297d3
adamhutchings <adam.abahot@gmail.com>
2020-03-19 14:29:48 -0700

    Create main file for game

---
+++ main/Asmura.java
@@ -0,0 +1,12 @@
+package main;
+
+import rendering.AFrame;
+
+public class Asmura {
+	
+	public static void main(String[] args) {
+		// Create an AFrame (window for the game)
+		new AFrame();
+	}
+
+}
