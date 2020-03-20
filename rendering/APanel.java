0e00726533bdf365efaea9a4f1ecbde560b819d1
adamhutchings <adam.abahot@gmail.com>
2020-03-20 14:57:35 -0700

    Add paintComponent method

--- rendering/APanel.java
+++ rendering/APanel.java
@@ -1,5 +1,17 @@
 package rendering;
 
-public class APanel {
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class APanel extends JPanel {
+	
+	// Repaint calls this with the appropriate Graphics object.
+	public void paintComponent(Graphics g) {
+		// Draw over the entire screen in white.
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, 800, 600);
+	}
 
 }
