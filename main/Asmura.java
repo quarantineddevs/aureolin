c877a665b9d3e597e5b007c296e3fd42da665fd6
adamhutchings <adam.abahot@gmail.com>
2020-03-20 15:04:08 -0700

    Add framerate and re-rendering

--- main/Asmura.java
+++ main/Asmura.java
@@ -1,12 +1,26 @@
 package main;
 
+import java.util.concurrent.TimeUnit;
+
 import rendering.AFrame;
 
 public class Asmura {
 	
+	private static int FRAMERATE = 30;
+	
 	public static void main(String[] args) {
 		// Create an AFrame (window for the game)
-		new AFrame();
+		AFrame frame = new AFrame();
+		// Repaint it forever
+		while (true) {
+			frame.repaint();
+			// Wait a certain amount
+			try {
+				TimeUnit.MILLISECONDS.sleep(1000/FRAMERATE);
+			} catch (InterruptedException exception) {
+				// TODO: Add error handling
+			}
+		}
 	}
 
 }
