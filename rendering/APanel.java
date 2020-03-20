3aaff07311841c0232f36c42d13eba97b663ce89
adamhutchings <adam.abahot@gmail.com>
2020-03-20 14:58:34 -0700

    Add background color(will changr with different screens)

--- rendering/APanel.java
+++ rendering/APanel.java
@@ -7,10 +7,13 @@ import javax.swing.JPanel;
 
 public class APanel extends JPanel {
 	
-	// Repaint calls this with the appropriate Graphics object.
+	private Color bgColor;
+	
+	// repaint calls this with the appropriate Graphics object.
+	@Override
 	public void paintComponent(Graphics g) {
 		// Draw over the entire screen in white.
-		g.setColor(Color.WHITE);
+		g.setColor(bgColor);
 		g.fillRect(0, 0, 800, 600);
 	}
 
