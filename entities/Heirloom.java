8fd1f6f5ee738a57946dc7cf7d04eb5c46a96961
Santiago Mackenzie <santiaghostbuster@gmail.com>
2020-04-05 20:58:20 -0700

    Add family heirloom

---
+++ entities/Heirloom.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class Heirloom extends PlotDriver {
+  String name = "Family Heirloom";
+  String description = "A small wooden carving in the shape of a turtle shell.";
+  
+  int itemID = 1;
+}
---
+++ entities/PlotDriver.java
@@ -0,0 +1,27 @@
+package entities;
+
+import animation.ErrorMessageEvent;
+import rendering.AFrame;
+
+public abstract class PlotDriver extends Item {
+  AFrame frame;
+  
+  String name;
+  String description;
+  
+  int itemID;
+  
+  void use(PlayerCharacter player) {
+    new ErrorMessageEvent(this.frame, "You can't use that here.");
+  }
+  
+  void useInBattle(PlayerCharacter player, Opponent enemy) {
+    new ErrorMessageEvent(this.frame, "You can't use that here.");
+  }
+  
+  @Override
+  
+  void remove(PlayerCharacter player) {
+    new ErrorMessageEvent(this.frame, "You can't remove this.");
+  }
+}
