9a6d0b6817e5d125e15c809978213b99078d9dbf
Santiago Mackenzie <santiaghostbuster@gmail.com>
2020-04-11 07:03:28 -0700

    Git is being bad

--- entities/BeginnersSword.java
+++ entities/BeginnersSword.java
@@ -5,4 +5,4 @@ public class BeginnersSword extends Weapon {
 	int attackPower = 2;
 	String name = "Beginner's Sword";
 	String description = "A simple and easy to use sword to designed for novices in combat to improve with";
-}
+} //git_fix
